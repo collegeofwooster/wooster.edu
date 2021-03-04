b7f00dee6d7656fc4c7e9ec1ceca0fb7b9569415
James Pederson <james@jpederson.com>
2021-03-04 16:36:34 -0600

    Add dashboard areas and metaboxes for faculty and areas of study.

--- functions.php
+++ functions.php
@@ -36,6 +36,9 @@ function require_multi( $files ) {
 require_multi( 'core', 'admin', 'metabox', 'emergency', 'images', 'paginate', 'metabox', 'showcase', 'featured-article', 'button' );
 
 
+require_multi( 'post-type/faculty', 'post-type/area' );
+
+
 // require composer autoload
 require_once 'vendor/autoload.php';
 
