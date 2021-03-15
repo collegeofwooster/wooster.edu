4ef60f48b2e83b4bb50cede61e59cbe3bc8e552c
James Pederson <james@jpederson.com>
2021-03-14 21:06:54 -0500

    Eliminate extraneous metaboxes on area of study editor.

--- library/post-type/area.php
+++ library/post-type/area.php
@@ -43,7 +43,7 @@ function area_post_type() {
 			'capability_type' => 'post',
 			'hierarchical' => false,
 			/* the next one is important, it tells what's enabled in the post editor */
-			'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'sticky')
+			'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'revisions')
 		) /* end of options */
 	); /* end of register post type */
 	
