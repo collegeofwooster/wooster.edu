b2d4a04243571f84a740d8ce04684e845097ee67
James Pederson <james@jpederson.com>
2021-05-18 14:39:22 -0500

    Add 'connect with wooster' menu to post page sidebar.

--- single.php
+++ single.php
@@ -20,55 +20,61 @@ get_header();
 				<p class="quiet">Posted in <?php print get_the_category_list( ', ' ) ?>.</p>
 			</div>
 			<div class="aside">
+			<?php
+			$cat_list = wp_get_post_categories( $post->ID );
+			$related_posts = get_posts( array(
+				'post_type' => 'post',
+				'cat__in' => $cat_list
+			) );
+			if ( !empty( $cat_list ) ) {
+				?>
+				<h3>Related Posts</h3>
+				<div class="articles list">
 				<?php
-				$cat_list = wp_get_post_categories( $post->ID );
-				$related_posts = get_posts( array(
-					'post_type' => 'post',
-					'cat__in' => $cat_list
-				) );
-				if ( !empty( $cat_list ) ) {
+				foreach ( $related_posts as $rpost ) {
 					?>
-					<h3>Related Posts</h3>
-					<div class="articles list">
+					<div class="entry"><a href="<?php print get_the_permalink( $rpost->ID ) ?>"><?php print $rpost->post_title ?></a></div>
 					<?php
-					foreach ( $related_posts as $rpost ) {
-						?>
-						<div class="entry"><a href="<?php print get_the_permalink( $rpost->ID ) ?>"><?php print $rpost->post_title ?></a></div>
-						<?php
-					}
+				}
+				?>
+				</div>
+				<?php
+			}
+
+			$tags = wp_get_post_tags( $post->ID );
+			$tag_array = array();
+			foreach ( $tags as $t ) {
+				$tag_array[] = str_replace( '-2', '', $t->slug );
+			}
+
+			if ( !empty( $tag_array ) ) {
+				$areas = get_posts( array(
+					'post_type' => 'area',
+					'post_name__in' => $tag_array
+				) );
+				?>
+				<br>
+				<h3>Related Areas of Study</h3>
+				<div class="areas list">
+				<?php
+				foreach ( $areas as $a ) {
 					?>
+					<div class="entry">
+						<a href="/area/<?php print $a->post_name ?>/"><h4><?php print $a->post_title ?></h4></a>
+						<p><?php print get_the_excerpt( $a->ID ); ?></p>
 					</div>
 					<?php
 				}
-
-				$tags = wp_get_post_tags( $post->ID );
-				$tag_array = array();
-				foreach ( $tags as $t ) {
-					$tag_array[] = str_replace( '-2', '', $t->slug );
-				}
-
-				if ( !empty( $tag_array ) ) {
-					$areas = get_posts( array(
-						'post_type' => 'area',
-						'post_name__in' => $tag_array
-					) );
-					?>
-					<br>
-					<h3>Related Areas of Study</h3>
-					<div class="areas list">
-					<?php
-					foreach ( $areas as $a ) {
-						?>
-						<div class="entry">
-							<a href="/area/<?php print $a->post_name ?>/"><h4><?php print $a->post_title ?></h4></a>
-							<p><?php print get_the_excerpt( $a->ID ); ?></p>
-						</div>
-						<?php
-					}
-					print "</ul>";
-				}
-					
 				?>
+				</div>
+				<?php
+			}
+				
+			?>
+				<br>
+				<h3>Connect with Wooster</h3>
+				<?php wp_nav_menu( array( 'menu' => 'connect-with-wooster' ) ); ?>
+
 			</div>
 		</div>
 				<?php
