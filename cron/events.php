071c0376754d29454f25a1230504d3a8164d66b9
James Pederson <james@jpederson.com>
2021-05-24 16:49:01 -0500

    Replace webservices api integration with a simple request to an available json feed.

--- cron/events.php
+++ cron/events.php
@@ -5,23 +5,10 @@
 include( '../../../../wp-load.php' );
 
 
-// check if we have 25live web services credentials in wp-config
-if ( !defined( '_25LWS_USER' ) || !defined( '_25LWS_PASS' ) ) {
-	die( json_encode( array( 'error' => 'no credentials set in config' ) ) );
-}
-
-// print _25LWS_USER . ":" . _25LWS_PASS; die;
+// pull events feed
+$events = file_get_contents( 'https://25livepub.collegenet.com/calendars/college-of-wooster-template-all-campus-events-2621.json' );
 
 
-// https://webservices.collegenet.com/r25ws/wrd/wooster/run/events.xml?query_id=4027&start_dt=20210523&otransform=json.xsl
-$ch = curl_init( 'https://webservices.collegenet.com/r25ws/wrd/wooster/run/events.xml?query_id=4027&start_dt=20210523&otransform=json.xsl' );
-
-curl_setopt( $ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/xml' ) );
-curl_setopt( $ch, CURLOPT_USERPWD, _25LWS_USER . ":" . _25LWS_PASS );
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE );
-$return = curl_exec( $ch );
-curl_close( $ch );
-
-
-print_r( $return );
+// dump for testing
+print_r( $events );
 
