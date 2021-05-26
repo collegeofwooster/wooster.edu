df640f799e4ef6c21a22a8c426e66599aa481fb4
James Pederson <james@jpederson.com>
2021-05-25 21:14:10 -0500

    Combine title ii and state performance report.

--- single-area.php
+++ single-area.php
@@ -69,8 +69,7 @@ $courses = get_cmb_value( "person_courses" );
 				<?php do_area_tab_nav( "Clinics", "clinics" ) ?>
 				<?php do_area_tab_nav( "Fern Valley Field Station", "fern_valley" ) ?>
 				<?php do_area_tab_nav( "Requirements", "requirements" ) ?>
-				<?php do_area_tab_nav( "Title II", "title_ii" ) ?>
-				<?php do_area_tab_nav( "State Performance Report", "state_performane" ) ?>
+				<?php do_area_tab_nav( "Title II and State Performance Report", "title_ii" ) ?>
 			</ul>
 		</div>
 		
@@ -151,8 +150,7 @@ $courses = get_cmb_value( "person_courses" );
 			<?php do_area_tab_content( "Clinics", "clinics" ); ?>
 			<?php do_area_tab_content( "Fern Valley Field Station", "fern_valley" ); ?>
 			<?php do_area_tab_content( "Requirements", "requirements" ) ?>
-			<?php do_area_tab_content( "Title II", "title_ii" ) ?>
-			<?php do_area_tab_content( "State Performance Report", "state_performane" ) ?>
+			<?php do_area_tab_content( "Title II and State Performance Report", "title_ii" ) ?>
 
 		<?php
 		endwhile;
