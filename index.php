464d268c0f2089d98763d06c40fcb8d00472d298
James Pederson <james@jpederson.com>
2021-06-01 11:31:22 -0500

    Update archive template and news template to use article cards and have a page header.

--- index.php
+++ index.php
@@ -5,37 +5,35 @@ Home/catch-all template
 
 get_header(); 
 
+the_page_header( "Wooster News", get_bloginfo('template_url') . '/img/bg-header-news.webp' );
+
 ?>
 
 	<div class="content-wide" role="main">
 
+		<div class="article-cards blog-listing">
 		<?php
-		if ( is_search() ) {
-			?><h1 class="post-title">Search Results for <span>'<?php print $_REQUEST["s"]; ?>'</span></h1><?php
-		} else {
-			?><h1 class="post-title">Our Blog</h1><?php
-		}
 
-		if ( have_posts() ) : 
+		while ( have_posts() ) : the_post();
+			?>
+			<div class="entry">
+				<div class="thumbnail">
+					<a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
+				</div>
+				<div class="entry-inner">
+					<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+					<?php the_excerpt(); ?>
+				</div>
+			</div>
+			<?php
+		endwhile;
 
-			// Start the Loop.
-			while ( have_posts() ) : the_post(); 
-				?>
-				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-				<?php the_excerpt(); ?>
-				<p class="quiet">Posted by <?php print get_the_author_link() ?> in <?php print get_the_category_list( ', ' ) ?>.</p>
-				<hr />
-				<?php
-			endwhile;
-
-		else :
-
-			print "<p>There are currently no posts to list here.</p>";
-
-		endif;
 		?>
-		
-		<?php pagination(); ?>
+		</div>
+
+		<div class="pagination">
+			<?php pagination(); ?>
+		</div>
 
 	</div><!-- #primary -->
 
