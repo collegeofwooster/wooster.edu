48c924c092c8da0d5024635f4ee29a83b043a753
James Pederson <james@jpederson.com>
2021-06-20 23:41:40 -0500

    Fix open and color on accordions.

--- library/accordion.php
+++ library/accordion.php
@@ -3,8 +3,8 @@
 
 function do_accordion( $title, $content, $color = 'grey-light', $open = 0 ) {
 	?>
-	<div class="accordion">
-		<div class="accordion-handle <?php print $color; ?><?php print ( $open ? 'open' : '' ) ?>">
+	<div class="accordion <?php print ( $open ? 'open' : '' ) ?>">
+		<div class="accordion-handle <?php print $color; ?>">
 			<?php print $title; ?>
 		</div>
 		<div class="accordion-content">
