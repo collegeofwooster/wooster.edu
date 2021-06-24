fa6d66eb575c1e083acbbfb840aa98baf63c0673
James Pederson <james@jpederson.com>
2021-06-23 21:34:09 -0500

    Fix areas of study column widths.

--- page-areas.php
+++ page-areas.php
@@ -6,26 +6,28 @@ Template Name: Areas of Study
 
 get_header();
 
+the_page_header(); 
+
 ?>
 	
-	<?php the_page_header(); ?>
-	
-	<div class="one-column areas">
-		<?php while ( have_posts() ) : the_post(); ?>
-		<div class="entry-content">
-			<?php the_content(); ?>
-		</div>
-		<?php endwhile; ?>
-		<div class="wrap group two-column academics">
-		
-			<div class="area-filter">
-				<select><option value="all">-- All Areas --</option><option value="ma">Major</option><option value="mi">Minor</option><option value="pa">Pre-Professional Advising</option><option value="tl">Teaching Licensure</option><option value="path">Pathway</option></select>
+	<div class="content-wide areas">
+		<div class="wrap">
+			<?php while ( have_posts() ) : the_post(); ?>
+			<div class="entry-content">
+				<?php the_content(); ?>
 			</div>
-			<hr />
-			<div class="group area-listing">
-				<?php list_area_category() ?>
-			</div>
+			<?php endwhile; ?>
+			<div class="academics">
+			
+				<div class="area-filter">
+					<select><option value="all">-- All Areas --</option><option value="ma">Major</option><option value="mi">Minor</option><option value="pa">Pre-Professional Advising</option><option value="tl">Teaching Licensure</option><option value="path">Pathway</option></select>
+				</div>
+				<hr />
+				<div class="area-listing">
+					<?php list_area_category() ?>
+				</div>
 
+			</div>
 		</div>
 	</div>
 
