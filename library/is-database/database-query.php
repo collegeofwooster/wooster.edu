ec9a7c526f004ecde5ad1499f7bdfa40b4eca308
James Pederson <james@jpederson.com>
2021-07-31 11:27:12 -0500

    Require the wp-config.php file in the IS database script so we have the credentials.

--- library/is-database/database-query.php
+++ library/is-database/database-query.php
@@ -3,6 +3,8 @@
 ini_set('display_errors', true);
 ini_set('error_reporting', E_ALL ^ E_NOTICE );
 
+require('../../../../../../wp-config.php');
+
 // variables
 $server = _IS_SERVER;
 $user = _IS_USER;
