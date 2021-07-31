a73fbddcb6f7c3d4c0028849e817a4440da70872
James Pederson <james@jpederson.com>
2021-07-30 21:10:34 -0500

    Add some more whitespace around accordion content, remove console logging from accordion js.

--- js/src/accordion.js
+++ js/src/accordion.js
@@ -5,7 +5,6 @@ jQuery(document).ready(function($){
 
 	if ( $( '.accordion' ).length > 0 ) {
 		$( '.accordion-handle' ).click(function(){
-			console.log( $(this).parent('.accordion') );
 			$( this ).parent('.accordion').toggleClass( 'open' );
 		});
 	}
