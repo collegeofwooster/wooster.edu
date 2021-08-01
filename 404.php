cce739bbf41c227f7642214f6467880ff2b6d275
James Pederson <james@jpederson.com>
2021-08-01 03:58:59 -0500

    Update the 404 template to correct the issue.

--- 404.php
+++ 404.php
@@ -9,8 +9,13 @@ get_header(); ?>
 	<div class="content-wide" role="main">
 		<div class="wrap">
 			<?php
-			$page = get_posts( 17245 );
+
+			// select the page they wanted for the 404
+			$page = get_post( 17245 );
+
+			// output the content
 			print apply_filters( 'the_content', $page->post_content );
+			
 			?>
 			<p class="search-form"><?php print get_search_form(); ?></p>
 		</div>
