f0c291402e2a3ddf7b330c761456c8ea60c3ec8a
James Pederson <james@jpederson.com>
2021-08-15 16:05:15 -0500

    Add directory shortcode that will utilize the directory files we pulled before.

---
+++ library/directory.php
@@ -0,0 +1,16 @@
+<?php
+
+
+// the directory shortcode
+function directory_shortcode() {
+
+	// get the table code 
+	$directory_table_code = file_get_contents( "../../../uploads/directory/directory.html" );
+
+	// return it
+	return $directory_table_code;
+
+}
+add_shortcode( 'directory', 'directory_shortcode' );
+
+
