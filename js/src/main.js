746a5aa22d2207fefbaedbef12ece8f74d0a9ede
James Pederson <james@jpederson.com>
2021-08-19 14:34:05 -0500

    Only execute fitvids after 5 seconds.

--- js/src/main.js
+++ js/src/main.js
@@ -11,9 +11,10 @@ jQuery(document).ready(function($){
 	$( '.lightbox-iframe, .lightbox-video, .video' ).magnificPopup({ 'type': 'iframe' });
 	$( '.lightbox' ).magnificPopup({ 'type': 'image' });
 
-	// add fitvids on anything in the content area.
-	$( '.content' ).fitVids();
-	$( '.left-column' ).fitVids();
+	// add fitvids on anything in the content area (after 5 seconds)
+	setTimeout(function(){
+		$( '.content' ).fitVids();
+	},5000);
 
 });
 
