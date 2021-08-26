a6a91148c38a1c9afeaab5fcac4a092582afb5a9
James Pederson <james@jpederson.com>
2021-08-25 21:43:00 -0500

    Update the zip search script.

--- library/counselors/adm-zip-query.php
+++ library/counselors/adm-zip-query.php
@@ -1,7 +1,7 @@
 <?php 
 
-// ini_set('display_errors', true);
-// ini_set('error_reporting', E_ALL ^ E_NOTICE );
+ini_set('display_errors', true);
+ini_set('error_reporting', E_ALL ^ E_NOTICE );
 
 // include core wp functionality
 require('../../../../../wp-config.php');
@@ -13,19 +13,21 @@ $db = _IS_DB;
 $port = _IS_PORT;
 $zipsearch = preg_replace('/[^A-Za-z0-9\-]/', '', $_REQUEST['q']);
 
-print $zipsearch; die;
 
 // connect
 $dbhandle = odbc_connect( "Driver={ODBC Driver 17 for SQL Server};SERVER=$server;DATABASE=$db;PORT=$port", $user, $pass )
 	or die( "Could not connect to $server" );
+
 
 // query
 $query = "SELECT username, staff_name, geomarket, zip ";
 $query .= "FROM x_adm_staff_assign_zip ";
 $query .= "WHERE zip = '$zipsearch' ";
 
+
 // execute
 $result = odbc_exec( $dbhandle, $query );
+
 
 // odbc_result_all($result);
 if ( odbc_num_rows( $result ) == 0 ) {
@@ -37,6 +39,7 @@ if ( odbc_num_rows( $result ) == 0 ) {
 		echo( $url );
 	}
 }
+
 
 // close the connection
 odbc_free_result( $result );
