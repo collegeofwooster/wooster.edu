9f60bf85abe6b6b9f029525857db101431dfd5cd
James Pederson <james@jpederson.com>
2021-10-28 20:44:51 -0500

    Add comments template to single posts and pages.

--- page.php
+++ page.php
@@ -20,6 +20,12 @@ the_showcase();
 		while ( have_posts() ) : the_post(); 
 			the_post_showcase();
 			the_content();
+					
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+			
 			the_boxes();
 			the_accordions();
 		endwhile;
