d8a632c83e5a5be94d969b2c624c1adceca3fc2e
James Pederson <james@jpederson.com>
2021-12-01 18:59:39 -0600

    Adjust the counselor search js to get the data via a function and log it to the console.

--- js/src/counselors.js
+++ js/src/counselors.js
@@ -1,11 +1,22 @@
 
 
 jQuery( document ).ready( function( $ ) {
+
+	// watch for a counselor search form submission
 	$('.counselor-search-form').submit(function(){
+
+		// prevent the form from actually going to the action url
 		event.preventDefault();
-		var zipcode = $('.zip-search').val();
-		var result = $.get( "/wp-content/themes/wooster/library/counselors/adm-zip-query.php?zip=" + zipcode );
-		console.log( result );
+
+		// get the zip from the field
+		var zipcode = $( '.zip-search' ).val();
+
+		// get the zip query page content which contains the counselor object
+		$.get( "/wp-content/themes/wooster/library/counselors/adm-zip-query.php?zip=" + zipcode, function( result ) {
+			console.log( result );
+		} );
+
 	});
+	
 });
 
