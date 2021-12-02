3a70e5cb40ac4695edf9e501400d22b7a8511814
James Pederson <james@jpederson.com>
2021-12-01 19:28:46 -0600

    Add js to handle the counselor result by either redirecting to bio page or displaying a generic error.

--- js/src/counselors.js
+++ js/src/counselors.js
@@ -13,7 +13,22 @@ jQuery( document ).ready( function( $ ) {
 
 		// get the zip query page content which contains the counselor object
 		$.get( "/wp-content/themes/wooster/library/counselors/adm-zip-query.php?zip=" + zipcode, function( result ) {
-			console.log( result );
+			
+			// parse the result into a usable js object
+			var data = JSON.parse( result );
+
+			// if the username isn't empty
+			if ( typeof( data.username ) != 'undefined' ) {
+
+				// redirect to the bio page for the counselor in question
+				location.href = '/bio/' + data.username;
+
+			} else {
+
+				$('.counselor-search-form').append( '<div class="error">No counselors found.</div>' );
+
+			}
+
 		} );
 
 	});
