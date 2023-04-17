cd50f9b87fd5870e575e1ebb0545f79187e97c88
James Pederson <james@jpederson.com>
2023-04-17 00:12:17 +0000

    Enable super and subscripts in styles.

--- library/scripts.php
+++ library/scripts.php
@@ -8,6 +8,23 @@ function p_scripts() {
 add_action( 'wp_enqueue_scripts', 'p_scripts' );
 
 
+
+
+// hooks your functions into the correct filters
+function btn_add_mce_button() {
+	// check user permissions
+	if ( !current_user_can( 'edit_posts' ) &&  !current_user_can( 'edit_pages' ) ) {
+		return;
+	}
+
+	// check if WYSIWYG is enabled
+	if ( 'true' == get_user_option( 'rich_editing' ) ) {
+		add_filter( 'mce_buttons', 'btn_register_mce_button' );
+	}
+}
+add_action('init', 'btn_add_mce_button');
+
+
 // register new button in the editor
 function btn_register_mce_button( $buttons ) {
 	array_push( $buttons, 'superscript' );
