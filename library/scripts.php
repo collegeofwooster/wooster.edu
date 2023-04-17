e643e129c3b1c6490b807f3618cd55c920fa2ec2
James Pederson <james@jpederson.com>
2023-04-17 00:03:59 +0000

    Add superscript button to the editor.

--- library/scripts.php
+++ library/scripts.php
@@ -6,3 +6,11 @@ function p_scripts() {
 	wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/js/main.js?v=17', array( 'jquery' ), false, true );
 }
 add_action( 'wp_enqueue_scripts', 'p_scripts' );
+
+
+// register new button in the editor
+function btn_register_mce_button( $buttons ) {
+	array_push( $buttons, 'superscript' );
+	//array_push( $buttons, 'btn_mce_button' );
+	return $buttons;
+}
