d31e232c4afca8f29194acf26e94fa5896bb40dd
James Pederson <james@jpederson.com>
2023-08-08 21:04:22 +0000

    Remove test function now that it's working.

--- library/directory.php
+++ library/directory.php
@@ -5,24 +5,11 @@
 function directory_shortcode() {
 
 	// get the table code 
-	$directory_table_code = file_get_contents( "https://wooster.edu/wp-content/uploads/directory/directory.html" );
-
-	// return it
-	return $directory_table_code;
-
-}
-add_shortcode( 'directory', 'directory_shortcode' );
-
-
-// the directory shortcode
-function directory_test_shortcode() {
-
-	// get the table code 
 	$directory_table_code = file_get_contents( "https://api.wooster.edu/directory/cache/directory.html" );
 
 	// return it
 	return $directory_table_code;
 
 }
-add_shortcode( 'directory-test', 'directory_test_shortcode' );
+add_shortcode( 'directory', 'directory_shortcode' );
 
