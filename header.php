20f289a687ecc3ad849b3357008552e0b5677a52
James Pederson <james@jpederson.com>
2024-01-04 20:19:07 +0000

    Update title bar content.

--- header.php
+++ header.php
@@ -6,7 +6,12 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width,initial-scale=1" />
 
-<title><?php wp_title( '&mdash;', true, 'right' ); ?></title>
+<title><?php 
+if ( !is_front_page() ) {
+	wp_title( '&mdash;', true, 'right' );
+} else {
+	bloginfo( 'sitename' );
+} ?></title>
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
