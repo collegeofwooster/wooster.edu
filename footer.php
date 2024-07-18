56b8f4f29a69070edbd6959664a2a6299c697891
James Pederson <james@jpederson.com>
2024-07-18 18:35:03 +0000

    Footer adjustments.

--- footer.php
+++ footer.php
@@ -26,15 +26,36 @@ $admin_email = get_option( 'admin_email' );
 				</div>
 
 				<div class="column">
-					<h4>Wooster Traditions</h4>
-					<div class="traditions">
-						<?php print get_snippet( 'footer-traditions', 0 ); ?>
-					</div>
-				</div>
-
-				<div class="column">
 					<h4>Quick Links</h4>
-					<?php wp_nav_menu( array( 'theme_location' => 'footer-links' ) ); ?>
+					<ul class="menu">
+					<?php 
+
+					// get the menu items to manually display (in two columns)
+					$menu_items = get_nav_menu_items_by_location( 'footer-links' );
+					if ( !empty( $menu_items ) ) {
+
+						// count for when we need to split columns
+						$half = ceil( count( $menu_items ) / 2 );
+
+						// loop through the menu items
+						$count = 1;
+						foreach ( $menu_items as $item ) {
+							// display the menu item
+							print '<li><a href="' . $item->url . '">' . $item->title . '</a></li>';
+
+							if ( $count == $half ) {
+								print '</ul></div><div class="column last"><ul class="menu">';
+							}
+
+							// increment count
+							$count++;
+						}
+
+					}
+
+					?>
+				
+
 				</div>
 
 			</div>
