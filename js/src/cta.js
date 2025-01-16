ee42633c5711c633dc2a62567d820c42cae982c3
James Pederson <james@jpederson.com>
2025-01-16 17:35:00 +0000

    Fix a cta js error for when there is no page header.

--- js/src/cta.js
+++ js/src/cta.js
@@ -2,20 +2,22 @@
 
 jQuery( document ).ready( function( $ ) {
 
-    var ctaScrollTarget = parseInt( $('.page-header-bottom').offset().top );
-    var menuHeight = parseInt( $('.content').css( 'margin-top' ) );
-    console.log( menuHeight );
+    var pageHeaderBottom = $('.page-header-bottom');
+    if ( pageHeaderBottom.length ) {
+        var ctaScrollTarget = parseInt( $('.page-header-bottom').offset().top );
+        var menuHeight = parseInt( $('.content').css( 'margin-top' ) );
+        console.log( menuHeight );
 
-    $(window).scroll(function(){
-        var scrollPosition = $(window).scrollTop();
-        if ( scrollPosition >= ( ctaScrollTarget - menuHeight ) ) {
-            $( '.cta-float' ).addClass( 'visible' );
-        } else {
-            $( '.cta-float' ).removeClass( 'visible' );
-        }
+        $(window).scroll(function(){
+            var scrollPosition = $(window).scrollTop();
+            if ( scrollPosition >= ( ctaScrollTarget - menuHeight ) ) {
+                $( '.cta-float' ).addClass( 'visible' );
+            } else {
+                $( '.cta-float' ).removeClass( 'visible' );
+            }
 
-    });
-
+        });
+    }
 
 });
 
